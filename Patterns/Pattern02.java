a20215a39649053f647909230ae54999c8e0a3b5
Arjun Patil <patilarjun2500@gmail.com>
2024-06-07 12:40:04 +0530

    Create Pattern02.java

---
+++ Patterns/Pattern02.java
@@ -0,0 +1,22 @@
+public class Pattern02 {
+  public static void main(String[] arjun) {
+    int i = 5;
+    while (i >= 1) {
+      int j = 1;
+      while (j <= i) {
+        System.out.print(" ");
+        j++;
+
+      }
+      int c = 5;
+      while (c >= j) {
+        System.out.print(" *");
+        c--;
+      }
+
+      System.out.println();
+      i--;
+    }
+
+  }
+}
