4b7d100d4adb8bf0e3268901a9e0b15bdf371add
Arjun Patil <patilarjun2500@gmail.com>
2024-06-23 12:02:50 +0530

    Create GCD.java

---
+++ DSA/GCD.java
@@ -0,0 +1,22 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class GCD {
+
+    static void gcd(int n1,int n2){
+        for(int i=1;i<=n1 && i<=n2;i++){
+
+        }
+    }
+     public static void main(String
+     
+     [] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println(" Enter the number number1 and number 2 ");
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+           
+        }
+    }
+}
